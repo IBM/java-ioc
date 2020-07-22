c4089c36691449556e54c706bb8157dab8fb7049
Manish Motwani <mmotwani@us.ibm.com>
2020-07-22 18:04:38 -0500

    Update header for Bindings.java

--- src/main/java/Bindings.java
+++ src/main/java/Bindings.java
@@ -1,21 +1,8 @@
-//
-// (C) Copyright IBM Corp. 2005 All Rights Reserved.
-//
-// Contact Information:
-//
-// IBM Corporation
-// Legal Department
-// 222 South Riverside Plaza
-// Suite 1700
-// Chicago, IL 60606, USA
-//
-// END-OF-HEADER
-//
-// -----------------------
-// @author: mmotwani
-//
-// Date: Jan 15, 2014
-// ---------------------
+/*
+* Copyright (c) IBM Corporation 2020. All Rights Reserved.
+* Project name: java-ioc
+* This project is licensed under the Apache License 2.0, see LICENSE.
+*/
 
 import com.ibm.ioc.JavaBindings;
 import com.ibm.ioc.JavaBindingsProvider.BindingsFactory;
